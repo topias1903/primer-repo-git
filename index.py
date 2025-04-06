e68c5a1727fcfd6b98a0d5cecd8d12dd849b4215
topias1903 <tobias_alamedas@hotmail.com>
2025-04-06 16:22:45 -0300

    mensaje

--- index.py
+++ index.py
@@ -2,5 +2,36 @@ print("Primer repo git")
 #git init
 #git add .
 #git commit -m "mensaje"
+#git status (para revisar si hay archivos sin subir)
 #git remote add origin ...
 #git push -u origin main
+
+#git add . (para actualizar los archivos del repo)
+#git commit -m "mensaje"
+#git push -u origin master
+
+#creacion de ramas
+#git branch (nos indica sobre que rama estamos y cuales estan creadas)
+#git branch "nombre rama" (crea una rama)
+#git checkout "nombre rama" (cambia de rama)
+#podemos crear otro archivo y cambiar la funcionalidad
+#git add .
+#git commit -m "mensaje"
+#git checkout main (cambiamos de rama y vemos que el archivo que creamos no aparece)
+#git checkout "nombre de rama" (volvemos a cambiar de rama y ver el archivo)
+
+#fusionar ramas
+#git checkout main
+#git merge "nombre rama a fusionar"
+
+#solucionar errores de sobreescritura de codigo con otro colaborador
+#git branch "nombre rama 3"
+#git checkout "nombre rama 3"
+#git add .
+#git commit -m "mensaje"
+#git checkout main
+#git add .
+#git commit -m "mensaje"
+#git merge "rama 3"
+#git add .
+#git commit -m "mensaje"
